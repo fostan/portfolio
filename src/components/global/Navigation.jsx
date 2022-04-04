a807ee3df095d5351a6ffa0876f94da25919679c
fostan <nathanielfostan@protonmail.com>
2022-04-03 20:39:33 -0500

    style updates

--- src/components/global/Navigation.jsx
+++ src/components/global/Navigation.jsx
@@ -4,10 +4,10 @@ import { Link } from "gatsby";
 const Navigation = () => {
   return (
     <header className="bg-gray-900 border-b-2 border-gray-800">
-      <div className="container max-w-6xl flex justify-center text-white mx-auto ">
+      <div className="container max-w-6xl flex justify-center text-sky-100 hover:text-white mx-auto ">
         <Link
           to="/"
-          className="-mb-[2px]	p-3 font-serif text-sm uppercase tracking-widest font-black border-b-2 opacity-50 border-transparent hover:border-primary hover:opacity-100 transition-all"
+          className="-mb-[2px]	p-3 font-serif text-sm uppercase tracking-widest font-black border-b-2 opacity-50 border-transparent hover:border-neutral-400 hover:opacity-100 transition-all"
         >
           Fostan
         </Link>
--- src/components/global/PortfolioCard.jsx
+++ src/components/global/PortfolioCard.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "gatsby";
+import { HiArrowSmRight } from "react-icons/hi";
 
 const PortfolioCard = ({ project }) => {
   const { title, client, tags, image, page } = project;
@@ -7,8 +8,9 @@ const PortfolioCard = ({ project }) => {
   return (
     <>
       <div className={`relative group `}>
-        <div className="text-gray-200 uppercase transition-all tracking-widest mb-4 text-xs opacity-75 group-hover:opacity-100">
+        <div className="text-sky-200 flex items-center gap-1 uppercase group transition-all tracking-widest mb-4 text-xs opacity-75 group-hover:opacity-100">
           {tags}
+          <HiArrowSmRight className="group-hover:translate-x-2 h-4 w-4 transition opacity-0 group-hover:opacity-100" />
         </div>
         <Link to={page} className="">
           <div className="relative bg-black h-[455px]  rounded-lg overflow-hidden group-hover:scale-[97%] ease-out duration-300 transition">
@@ -22,15 +24,17 @@ const PortfolioCard = ({ project }) => {
                 height="455px"
               />
               <div
-                className="absolute inset-0 bg-gradient-to-t from-gray-900 to-transparent"
+                className="absolute inset-0 bg-gradient-to-t from-neutral-900 to-transparent"
                 aria-hidden="true"
               />
             </div>
             <div className="relative max-w-7xl p-6 h-full  flex flex-col justify-end">
-              <p className="text-gray-200 uppercase group-hover:text-white transition tracking-widest text-sm">
+              <p className="text-sky-200 uppercase group-hover:text-white transition tracking-widest text-sm">
                 {client}
               </p>
-              <h1 className="text-3xl font-black text-white mb-0">{title}</h1>
+              <h1 className="text-3xl font-black text-gray-100 mb-0">
+                {title}
+              </h1>
             </div>
           </div>
         </Link>
